6c6f2f68b0886198fd665a88dc4bd5fcfd969fcc
Whaeedil <baoppyhossen1234@gmail.com>
2022-04-18 01:21:33 +0600

    card-done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Services from '../Services/Services';
 
-const services = [
-    { id: 1, name: 'one', price: 100, description: '', img: '' }
-]
-
 const Home = () => {
     return (
         <div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Home/Service/Service';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
-        <div>
-            <h2>Services</h2>
-        </div>
+        <div id='Services' className='container' >
+            <h2 className='text-primary text-center mt-5'>Our  Services</h2>
+            <div className="row">
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+        </div >
+
     );
 };
 
